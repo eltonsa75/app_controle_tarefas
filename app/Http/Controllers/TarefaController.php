0b5bc164a08f8be29b6c4accca4019544162dc3b
Elton Andrade <eltonsa75@hotmail.com>
2022-05-31 16:14:43 -0300

    Ajustando a quantidade de registo na paginação.

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -24,7 +24,7 @@ class TarefaController extends Controller
     public function index()
     {
         $user_id = auth()->user()->id;
-        $tarefas = Tarefa::where('user_id', $user_id)->paginate(1);
+        $tarefas = Tarefa::where('user_id', $user_id)->paginate(10);
         return view('tarefa.index', ['tarefas' => $tarefas]);
     }
 
