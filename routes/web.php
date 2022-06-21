636dab6dc70a0541d7faaef1290d590b81b52527
Elton <eltonsa75@hotmail.com>
2022-06-21 17:18:15 -0300

    Atualizando o Git

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 Auth::routes(['verify' => true]);
 
 /* Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
-->name('home')
+->name('home - teste')
 ->middleware('verified');
  */
 
