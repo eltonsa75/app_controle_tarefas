6c17ea856c3d0aa444a7678c856bab5edc8434f5
Elton <eltonsa75@hotmail.com>
2022-06-21 21:51:19 -0300

    Validando se a tarefa pertence ao usuário antes de habilitar a edição.

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -78,7 +78,13 @@ class TarefaController extends Controller
      */
     public function edit(Tarefa $tarefa)
     {
-        return view('tarefa.edit', ['tarefa' => $tarefa]);
+        $user_id = auth()->user()->id;
+
+       if($tarefa->user_id == $user_id){
+           return view('tarefa.edit', ['tarefa' => $tarefa]);
+       }
+
+       return view('acesso-negado');
     }
 
     /**
@@ -90,8 +96,14 @@ class TarefaController extends Controller
      */
     public function update(Request $request, Tarefa $tarefa)
     {
+
+        if(!$tarefa->user_id == $user_id = auth()->user()->id){
+            return view('acesso-negado');
+        }
+
         $tarefa->update($request->all());
         return redirect()->route('tarefa.show', ['tarefa' => $tarefa->id]);
+
     }
 
     /**
---
+++ resources/views/acesso-negado.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Acesso Negado</div>
+
+                <div class="card-body">
+                  Desculpa. Você não tem acesso a esse recurso.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
