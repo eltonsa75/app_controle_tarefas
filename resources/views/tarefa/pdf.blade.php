9dd15a7f1cd2aed53807c51f3480c3135c5518ae
Elton <eltonsa75@hotmail.com>
2022-06-27 10:00:55 -0300

    Adicionando suporte UTF-8, estilos CSS e quebras de páginas nas Views de PDF

--- resources/views/tarefa/pdf.blade.php
+++ resources/views/tarefa/pdf.blade.php
@@ -1,22 +1,59 @@
-<h2>Lista de tarefas</h2>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
-<table>
-    <thead>
-        <tr></tr>
-            <th>ID</th>
-            <th>Tarefa</th>
-            <th>Data limite conclusão</th>
-        </tr>
-    </thead>
+        <style>
+            .page-break {
+                page-break-after: always;
+            }
 
-    <tbody>
-        @foreach ($tarefas as $key => $tarefa)
-            <tr>
-                <td>{{ $tarefa->id }}</td>
-                <td>{{ $tarefa->tarefa }}</td>
-                <td>{{ date('d/m/Y', strtotime($tarefa->data_limite_conclusao)) }}</td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+            .titulo {
+                border:1px;
+                background-color:#c2c2c2;
+                text-align:center;
+                width:100%;
+                text-transform:uppercase;
+                font-weight:bold;
+                margin-bottom:25px;
+            }
 
+            table th {
+                text-align:left;
+            }
+        </style>
+    </head>
+
+    <body>
+
+        <div class="titulo">Lista de tarefas</div>
+
+        <table style="width:100%">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Tarefa</th>
+                    <th>Data limite conclusão</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach($tarefas as $key => $tarefa)
+                    <tr>
+                        <td>{{ $tarefa->id }}</td>
+                        <td>{{ $tarefa->tarefa }}</td>
+                        <td>{{ date('d/m/Y', strtotime($tarefa->data_limite_conclusao)) }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        <div class="page-break"></div>
+        <h2>Página 2</h2>
+
+        <div class="page-break"></div>
+        <h2>Página 3</h2>
+
+        <div class="page-break"></div>
+        <h2>Página 3</h2>
+    </body>
+</html>
