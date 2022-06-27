47cf6e3c67c0facb1a595fb9f34e9fbf45816363
Elton <eltonsa75@hotmail.com>
2022-06-27 10:08:27 -0300

    Lavaravel DOMPDF - Definindo o tipo de papel e a orientação da impressão.

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -139,6 +139,8 @@ class TarefaController extends Controller
 
         $tarefas = auth()->user()->tarefas()->get();
         $pdf = PDF::loadView('tarefa.pdf', ['tarefas' => $tarefas]);
+
+        $pdf->setPaper('A4', 'landscape');
         //return $pdf->download('lista_de_tarefas.pdf');
         return $pdf->stream('lista_de_tarefas.pdf');
     }
