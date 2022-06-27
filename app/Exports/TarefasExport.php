1db0e095e7a1712909e36286c7abb6fa9468cc71
Elton <eltonsa75@hotmail.com>
2022-06-27 09:21:25 -0300

    Configurando o pacote DomPDF

--- app/Exports/TarefasExport.php
+++ app/Exports/TarefasExport.php
@@ -5,8 +5,9 @@ namespace App\Exports;
 use App\Models\Tarefa;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class TarefasExport implements FromCollection, WithHeadings
+class TarefasExport implements FromCollection, WithHeadings, WithMapping
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -21,11 +22,18 @@ class TarefasExport implements FromCollection, WithHeadings
     {
         return [
             'ID da Tarefa',
-            'ID do Usuário',
             'Tarefa',
             'Data Limite Conclusão',
-            'Data Criação',
-            'Data atualização',
+
+        ];
+    }
+
+    public function map($linha): array
+    {
+        return [
+            $linha->id,
+            $linha->tarefa,
+            date('d/m/Y', strtotime($linha->data_limite_conclusao)),
         ];
     }
 }
--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
     ],
 
@@ -228,6 +229,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
